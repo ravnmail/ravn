12201f7a0fd5596ea12164c3cdaa8cc9934fd036
Michael Wallner <m.wallner@coderscantina.com>
2025-12-03 15:09:59 +0100

    👔 add dropdown to EmailAddress to copy email

--- app/components/Ravn/EmailAddress.vue
+++ app/components/Ravn/EmailAddress.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
-
 import type { EmailAddress } from '~/types/email'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '~/components/ui/dropdown-menu'
+
+const { copy } = useClipboard()
+
 interface Props extends EmailAddress {
   isLast?: boolean
   showAvatar?: boolean
@@ -16,20 +19,26 @@ withDefaults(defineProps<Props>(), {
 </script>
 
 <template>
-  <div
-    class="relative inline-flex items-center space-x-2 group text-foreground hover:bg-selection hover:text-selection-foreground hover:z-10 pl-1 py-0.5 -mx-1 -my-0.5 rounded pr-5"
-  >
-    <RavnAvatar
-      v-if="showAvatar"
-      :account-id="accountId"
-      :email="address"
-      :name="name"
-      size="xs"
-    />
-    {{ name || address }}
-    <Icon
-      class="absolute right-1 opacity-0 group-hover:opacity-100"
-      name="lucide:chevron-down"
-    />
-  </div>
+  <DropdownMenu>
+    <DropdownMenuTrigger
+      class="relative inline-flex items-center space-x-2 group text-foreground hover:bg-selection hover:text-selection-foreground hover:z-10 pl-1 py-0.5 -mx-1 -my-0.5 rounded pr-5">
+      <RavnAvatar
+        v-if="showAvatar"
+        :account-id="accountId"
+        :email="address"
+        :name="name"
+        size="xs"
+      />
+      <span>{{ name || address }}</span>
+      <Icon
+        class="absolute right-1 opacity-0 group-hover:opacity-100"
+        name="lucide:chevron-down"
+      />
+    </DropdownMenuTrigger>
+    <DropdownMenuContent>
+      <DropdownMenuItem @click="copy(address)">
+        <span>{{ address }}</span>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
 </template>
